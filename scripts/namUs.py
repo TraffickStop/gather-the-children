841f97966caf6c3048bee76d551fd5da3fd787cb
Spencer Fitzgerald <spencerfitz0196@gmail.com>
2021-02-01 13:14:29 -0700

    make command line tool

--- scripts/namUs.py
+++ scripts/namUs.py
@@ -9,7 +9,6 @@ import getopt, pdb, re, sys, time
 
 # CONSTANTS
 CASE_NUMBER_KEY = 'Case Number'
-STATES = ['Utah']
 IMG_COLUMNS = [
     'Case Number',
     'Image Link'
@@ -36,7 +35,9 @@ options.add_argument('--incognito')
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 
-def apply_state_filters():
+def apply_state_filters(states):
+    print('Adding selected states to filter...')
+
     circumstances_section = driver.find_element_by_id('Circumstances')
     labels_in_section = circumstances_section.find_elements_by_tag_name('label')
 
@@ -46,39 +47,17 @@ def apply_state_filters():
         if (label.text == "State"):
             state_input_box = label.find_element_by_tag_name('input')
             # add state filter
-            for state in STATES:
+            for state in states:
                 state_input_box.send_keys(state)
                 state_input_box.send_keys(Keys.ENTER)
 
-    # navigate to list view
     action_buttons = driver.find_elements_by_class_name('button-box')[0].find_elements_by_tag_name('input')
     action_buttons[1].click()
     time.sleep(1.5)
 
-def rows_to_show(num_rows):
-    results_selection_dropdown = driver.find_element_by_xpath("//label/span[contains(text(),'Results')]/following-sibling::select")
-    Select(results_selection_dropdown).select_by_value(f'{num_rows}')
-    time.sleep(1.5)
-
-def get_info_results():
-    # navigate to list view
-    driver.find_element_by_xpath("//i[@class=\"icon-list\"]").click()
-    time.sleep(1.5)
-
-    info_df = pd.DataFrame(columns=INFO_COLUMNS)
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-    rows = soup.find('div', class_='ui-grid-canvas').contents
-
-    for row in rows:
-        if row != ' ':
-            cells = row.find_all('div', class_='ui-grid-cell-contents')
-            cells_text = map(lambda cell: cell.text.strip(), cells)
-            new_info_df = pd.DataFrame([list(cells_text)], columns=INFO_COLUMNS)
-            info_df = info_df.append(new_info_df, ignore_index=True)
-    
-    return info_df
-
 def get_image_results():
+    print('Gathering images...')
+
     # navigate to grid view
     driver.find_element_by_xpath("//i[@class=\"icon-grid-six\"]").click()
     time.sleep(1.5)
@@ -97,28 +76,66 @@ def get_image_results():
 
     return image_df
 
-def main():
+def get_info_results():
+    print('Gathering info...')
+
+    # navigate to list view
+    driver.find_element_by_xpath("//i[@class=\"icon-list\"]").click()
+    time.sleep(1.5)
+
+    info_df = pd.DataFrame(columns=INFO_COLUMNS)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    rows = soup.find('div', class_='ui-grid-canvas').contents
+
+    for row in rows:
+        if row != ' ':
+            cells = row.find_all('div', class_='ui-grid-cell-contents')
+            cells_text = map(lambda cell: cell.text.strip(), cells)
+            new_info_df = pd.DataFrame([list(cells_text)], columns=INFO_COLUMNS)
+            info_df = info_df.append(new_info_df, ignore_index=True)
+    
+    return info_df
+
+def parse_args(argv):
+    help_message = 'Example usage: namUs.py --states=Utah,Arizona,California\n\nOptions:\n\t--states=: states flag; accepts a comma-separated list ie.(--states=Arizona,Utah,California)\n\t-h : Shows this help Screen; can also use --help'
+
+    try:
+        opts, args = getopt.getopt(argv,'h',['help', 'states='])
+    except getopt.GetoptError:
+        print(help_message)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h','--help'):
+            print(help_message)
+            sys.exit()
+        elif opt == '--states':
+            return arg.split(',')
+
+def rows_to_show(num_rows):
+    print(f'Setting {MAX_ROWS_PER_PAGE} rows per page...')
+
+    results_selection_dropdown = driver.find_element_by_xpath("//label/span[contains(text(),'Results')]/following-sibling::select")
+    Select(results_selection_dropdown).select_by_value(f'{num_rows}')
+    time.sleep(1.5)
+
+def main(argv):
+    states = parse_args(argv)
+    
     print('Navigating to namus.gov...')
     driver.get("https://www.namus.gov/MissingPersons/Search")
 
-    print('Adding selected states to filter...')
-    apply_state_filters()
-
-    print(f'Setting {MAX_ROWS_PER_PAGE} rows per page...')
+    apply_state_filters(states)
+    # TODO: for loop through the pages
     rows_to_show(MAX_ROWS_PER_PAGE)
-
-    print('Gathering info...')
     info_df = get_info_results()
-
-    print('Gathering images...')
     image_df = get_image_results()
 
     print('Merging Data...')
     merged_df = pd.merge(left=info_df, right=image_df, left_on=CASE_NUMBER_KEY, right_on=CASE_NUMBER_KEY)
 
-    pdb.set_trace()
-
+    # TODO: send dataframe info to db
     driver.quit()
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    main(sys.argv[1:])
